7446cd15e4a7e34aa731336e9f0f1fa26f83a506
Sadhyama Vengilat <Sadhyama_Vengilat2@cable.comcast.com>
2022-03-25 20:16:33 +0530

    Outparams new value

--- src/xmidtsend_rbus.c
+++ src/xmidtsend_rbus.c
@@ -399,12 +399,13 @@ void createOutParamsandSendAck(wrp_msg_t *msg, rbusMethodAsyncHandle_t asyncHand
 	//rbusObject_SetValue(outParams, "msg_type", value1);
 	//rbusValue_Release(value1);
 
-	rbusValue_Init(&value);
-	rbusValue_SetString(value, msg->u.event.source);
-	rbusObject_SetValue(outParams, "source", value);
-	rbusValue_Release(value);
+	ParodusInfo("msg->u.event.source is %s\n", msg->u.event.source);
+	rbusValue_Init(&value1);
+	rbusValue_SetString(value1, msg->u.event.source);
+	rbusObject_SetValue(outParams, "source", value1);
+	rbusValue_Release(value1);
 
-	rbusValue_Init(&value);
+	/*rbusValue_Init(&value);
 	rbusValue_SetString(value, msg->u.event.dest);
 	rbusObject_SetValue(outParams, "dest", value);
 	rbusValue_Release(value);
@@ -424,12 +425,12 @@ void createOutParamsandSendAck(wrp_msg_t *msg, rbusMethodAsyncHandle_t asyncHand
 	rbusValue_Release(value);
 
 	ParodusInfo("statuscode %d errorMsg %s\n", statuscode, errorMsg);
-	/*rbusValue_Init(&value);
+	rbusValue_Init(&value);
 	rbusValue_SetInt32(value, statuscode);
 	rbusObject_SetValue(outParams, "status", value);
 	rbusValue_Release(value);*/
 
-	rbusValue_Init(&value);
+	/*rbusValue_Init(&value);
 	rbusValue_SetString(value, errorMsg);
 	rbusObject_SetValue(outParams, "error_message", value);
 	rbusValue_Release(value);
@@ -437,9 +438,9 @@ void createOutParamsandSendAck(wrp_msg_t *msg, rbusMethodAsyncHandle_t asyncHand
 	rbusValue_Init(&value);
 	rbusValue_SetString(value, "transaction_uuid"); //change this to actual transid
 	rbusObject_SetValue(outParams, "transaction_uuid", value);
-	rbusValue_Release(value);
+	rbusValue_Release(value);*/
 
-	ParodusInfo("multiple createOutParams done\n");
+	ParodusInfo("2 new createOutParams done\n");
 
 	if(outParams !=NULL)
 	{
